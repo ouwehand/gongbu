7613d31f341af9aa2493a908894f4b3ca1c88eeb
ouwehand <ouwehand@github.com>
2017-05-23 17:55:45 +0200

    generating a wordlist (test+implementation)

---
+++ src/main/python/gongbu.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import random
+import sqlite3
+from abc import ABCMeta, abstractmethod
+from collections import defaultdict
+
+def generate_wordlist(cursor, active_categories, english_to_korean=False):
+    """Generating a wordlist corresponding to selected categories"""
+
+    defn_dict = {}
+
+    # Empty set of active categories means that we get *all* the words
+
+    if active_categories:
+        cursor.execute('select * from Definitions')
+        for v in cursor.fetchall():
+            defn_dict[v[0]] = (v[1], v[2])
+    else:
+        for s in active_categories:
+            for v in s.getWords(cursor):
+                defn_dict[v[0]] = (v[1], v[2])
+
+    if english_to_korean:
+        return [(defn_dict[word][1], [defn_dict[word][0]])
+                for word in defn_dict]
+    else:
+        return collect_homonyms(defn_dict)
+
+
+def collect_homonyms(defn_dict):
+    """Deal with homonyms: concatenate the English translations."""
+
+    korean_dict = defaultdict(list)
+
+    for key in defn_dict:
+        korean_word = defn_dict[key][0]
+        english_translation = defn_dict[key][1]
+        korean_dict[korean_word].append(english_translation)
+
+    return [(korean_word, korean_dict[korean_word])
+            for korean_word in korean_dict]
---
+++ src/unittest/python/core_tests.py
@@ -0,0 +1,34 @@
+import unittest
+import unittest.mock as mock
+import gongbu
+from collections import namedtuple
+import sqlite3
+
+
+class TestGenWordlist(unittest.TestCase):
+
+    def test_collect_homonyms(self):
+        """Test for auxiliary function collect_homonyms"""
+        defn_dict = {
+                1: ('ANY_KOREAN_WORD_1', 'ANY_ENGLISH_WORD_1'),
+                2: ('ANY_KOREAN_WORD_2', 'ANY_ENGLISH_WORD_2'),
+                3: ('ANY_KOREAN_WORD_1', 'ANY_ENGLISH_WORD_3')}
+        expected = [
+                ('ANY_KOREAN_WORD_1',
+                    ['ANY_ENGLISH_WORD_1', 'ANY_ENGLISH_WORD_3']),
+                ('ANY_KOREAN_WORD_1',
+                    ['ANY_ENGLISH_WORD_2'])]
+        received = gongbu.collect_homonyms(defn_dict)
+        self.assertEqual(received, expected)
+
+    def test_active_categories_empty_e2k_true(self):
+        mock_cursor = mock.Mock()
+        mock_cursor.fetchall.return_value = [
+                (1, 'ANY_KOREAN_WORD_1', 'ANY_ENGLISH_WORD_1'),
+                (2, 'ANY_KOREAN_WORD_2', 'ANY_ENGLISH_WORD_2')]
+
+        expected = [('ANY_ENGLISH_WORD_1', ['ANY_KOREAN_WORD_1']),
+                    ('ANY_ENGLISH_WORD_2', ['ANY_KOREAN_WORD_2'])]
+
+        received = gongbu.generate_wordlist(mock_cursor, set(), True)
+        self.assertEqual(received, expected)
