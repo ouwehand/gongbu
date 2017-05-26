6913bf3f59f8ef9d947038594548d01a202e292a
ouwehand <ouwehand@github.com>
2017-05-26 19:17:55 +0200

    added sql folder

---
+++ src/sql/database_setup.sql
@@ -0,0 +1,75 @@
+.mode column
+pragma foreign_keys = 1;
+
+--Contains Korean words and English translations
+
+CREATE TABLE definitions(
+	defnID INTEGER PRIMARY KEY,
+	korean TEXT NOT NULL,
+	english TEXT NOT NULL
+);
+
+--Assign numeric identifiers to all the available labels
+
+CREATE TABLE label_descriptions(
+	lblID INTEGER PRIMARY KEY,
+	description TEXT
+);
+
+INSERT INTO label_descriptions (description) VALUES ("Language test vocabulary");
+
+--Assign one or more labels to a word
+
+CREATE TABLE labels(
+	defnID INTEGER NOT NULL,
+	lblID INTEGER NOT NULL,
+
+	PRIMARY KEY (defnID, lblID),
+	FOREIGN KEY (defnID) REFERENCES definitions (defnID),
+	FOREIGN KEY (lblID) REFERENCES label_descriptions (lblID)
+);
+
+--Assign numeric identifiers to all the available properties
+
+CREATE TABLE property_descriptions(
+	propID INTEGER PRIMARY KEY,
+	prop_description TEXT
+);
+
+INSERT INTO property_descriptions (propID, prop_description) VALUES (1, "Regularity of verbs/adjectives");
+
+--Assign numeric identifiers to all the states that a property may have
+
+CREATE TABLE state_descriptions(
+	stateID INTEGER PRIMARY KEY,
+	state_description TEXT
+);
+
+INSERT INTO state_descriptions (stateID, state_description) VALUES (1, "Regular verb/adjective");
+INSERT INTO state_descriptions (stateID, state_description) VALUES (2, "Irregular verb/adjective");
+
+--This table defines the available properties (i.e. their valid states)
+
+CREATE TABLE property_definitions(
+	propID INTEGER NOT NULL,
+	stateID INTEGER NOT NULL,
+
+	PRIMARY KEY (propID, stateID),
+	FOREIGN KEY (propID) REFERENCES property_descriptions (propID),
+	FOREIGN KEY (stateID) REFERENCES state_descriptions (stateID)
+);
+
+INSERT INTO property_definitions (propID, stateID) VALUES (1,1);
+INSERT INTO property_definitions (propID, stateID) VALUES (1,2);
+
+--Assign one or more properties (each property with its state) to a word
+
+CREATE TABLE properties(
+	defnID INTEGER NOT NULL,
+	propID INTEGER NOT NULL,
+	stateID INTEGER NOT NULL,
+
+	PRIMARY KEY (defnID, propID),
+	FOREIGN KEY (defnID) REFERENCES definitions (defnID),
+	FOREIGN KEY (propID, stateID) REFERENCES property_definitions (propID, stateID)
+);
