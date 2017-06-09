1280ad959a02b5dd5b3519171365bd4717b8ecfc
ouwehand <ouwehand@github.com>
2017-06-09 19:13:11 +0200

    add one more integration test

--- src/integrationtest/python/database_tests.py
+++ src/integrationtest/python/database_tests.py
@@ -29,10 +29,56 @@ class TestLabelDatabaseInteraction(unittest.TestCase):
         self.cursor.close()
         self.connection.close()
 
-    def test_label_database(self):
-        label = gongbu.Label(42, 'ANY_DESCR')
+    def test_label_database_interaction(self):
+        label = gongbu.Label(42, 'ANY_LABEL')
 
         received = label.get_words(self.cursor)
+        expected = [(1000, 'ANY_KOREAN', 'ANY_ENGLISH')]
+
+        self.assertEqual(expected, received)
+
+
+class TestStatelDatabaseInteraction(unittest.TestCase):
+
+    def setUp(self):
+        self.connection = sqlite3.connect('./src/sql/gongbu.db')
+        self.connection.isolation_level = None
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute('BEGIN TRANSACTION')
+
+        self.cursor.execute('INSERT INTO state_descriptions '
+                            '(stateID, state_description) VALUES '
+                            '(42, "ANY_STATE")')
+
+        self.cursor.execute('INSERT INTO property_descriptions '
+                            '(propID, prop_description) VALUES '
+                            '(110, "PROPERTY_1"), '
+                            '(111, "PROPERTY_2")')
+
+        self.cursor.execute('INSERT INTO property_definitions '
+                            '(propID, stateID) VALUES '
+                            '(110, 42), '
+                            '(111, 42)')
+
+        self.cursor.execute('INSERT INTO definitions '
+                            '(defnID, korean, english) VALUES '
+                            '(1000, "ANY_KOREAN", "ANY_ENGLISH")')
+
+        self.cursor.execute('INSERT INTO properties '
+                            '(defnID, propID, stateID) VALUES '
+                            '(1000, 110, 42), '
+                            '(1000, 111, 42)')
+
+    def tearDown(self):
+        self.cursor.execute('ROLLBACK')
+        self.cursor.close()
+        self.connection.close()
+
+    def test_state_database_interaction(self):
+        state = gongbu.State(42, 'ANY_STATE')
+
+        received = state.get_words(self.cursor)
         expected = [(1000, 'ANY_KOREAN', 'ANY_ENGLISH')]
 
         self.assertEqual(expected, received)
