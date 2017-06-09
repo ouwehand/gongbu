93a09dd65d8c708a23d190cb3b68f5e61ae21fd6
ouwehand <ouwehand@github.com>
2017-06-09 22:46:47 +0200

    eliminated some duplication in SQL queries

--- src/sql/import_cards.sql
+++ src/sql/import_cards.sql
@@ -50,22 +50,22 @@ INSERT INTO properties (defnID, propID, stateID) SELECT defnID, 1, 1 FROM defini
 --De-couple a few Korean homonyms
 
 DELETE FROM definitions WHERE korean="밤";
-INSERT INTO definitions (korean, english) VALUES ("밤", "night");
-INSERT INTO definitions (korean, english) VALUES ("밤", "chestnut");
+INSERT INTO definitions (korean, english) VALUES ("밤", "night"),
+                                                 ("밤", "chestnut");
 
 DELETE FROM definitions WHERE korean="병";
-INSERT INTO definitions (korean, english) VALUES ("병", "bottle");
-INSERT INTO definitions (korean, english) VALUES ("병", "illness");
+INSERT INTO definitions (korean, english) VALUES ("병", "bottle"),
+                                                 ("병", "illness");
 
 --Not a true homonym, but an abbreviation
 
 DELETE FROM definitions WHERE korean="차";
-INSERT INTO definitions (korean, english) VALUES ("차", "tea");
-INSERT INTO definitions (korean, english) VALUES ("차 / 자동차", "car");
+INSERT INTO definitions (korean, english) VALUES ("차", "tea"),
+                                                 ("차 / 자동차", "car");
 
 --Add a few more Korean homonyms
 
-INSERT INTO definitions (korean, english) VALUES ("쓰다", "to wear something on the head (hat or glasses)");
-INSERT INTO definitions (korean, english) VALUES ("쓰다", "bitter");
+INSERT INTO definitions (korean, english) VALUES ("쓰다", "to wear something on the head (hat or glasses)"),
+                                                 ("쓰다", "bitter");
 
 COMMIT;
