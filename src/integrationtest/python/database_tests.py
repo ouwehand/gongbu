40ee20fe2f9832bcaafb7035e69276c17dd3eeae
ouwehand <ouwehand@github.com>
2017-06-16 12:27:47 +0200

    added more integration tests

--- src/integrationtest/python/database_tests.py
+++ src/integrationtest/python/database_tests.py
@@ -3,6 +3,69 @@ import gongbu
 import sqlite3
 
 
+class TestDatabaseRules(unittest.TestCase):
+
+    def setUp(self):
+        self.connection = sqlite3.connect('./src/sql/gongbu.db')
+        self.connection.isolation_level = None
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute('PRAGMA foreign_keys=1')
+
+        self.cursor.execute('BEGIN TRANSACTION')
+
+        self.cursor.execute('INSERT INTO state_descriptions '
+                            '(stateID, state_description) VALUES '
+                            '(42, "ANY_STATE_1"), '
+                            '(43, "ANY_STATE_2")')
+
+        self.cursor.execute('INSERT INTO property_descriptions '
+                            '(propID, prop_description) VALUES '
+                            '(110, "PROPERTY_1"), '
+                            '(111, "PROPERTY_2")')
+
+        self.cursor.execute('INSERT INTO property_definitions '
+                            '(propID, stateID) VALUES '
+                            '(110, 42), '
+                            '(110, 43), '
+                            '(111, 42)')
+
+        self.cursor.execute('INSERT INTO definitions '
+                            '(defnID, korean, english) VALUES '
+                            '(1000, "ANY_KOREAN_1", "ANY_ENGLISH_1"), '
+                            '(1001, "ANY_KOREAN_2", "ANY_ENGLISH_2")')
+
+        self.cursor.execute('INSERT INTO properties '
+                            '(defnID, propID, stateID) VALUES '
+                            '(1000, 110, 42), '
+                            '(1000, 111, 42)')
+
+    def tearDown(self):
+        self.cursor.execute('ROLLBACK')
+        self.cursor.close()
+        self.connection.close()
+
+    def test_state_must_be_assigned_to_property(self):
+        with self.assertRaises(sqlite3.IntegrityError) as cm:
+
+            self.cursor.execute('INSERT INTO properties '
+                                '(defnID, propID, stateID) VALUES '
+                                '(1001, 111, 43)')
+
+        self.assertEqual(str(cm.exception), 'FOREIGN KEY constraint failed')
+
+    def test_property_must_have_unique_state(self):
+        with self.assertRaises(sqlite3.IntegrityError) as cm:
+
+            self.cursor.execute('INSERT INTO properties '
+                                '(defnID, propID, stateID) VALUES '
+                                '(1000, 110, 43)')
+
+        self.assertEqual(str(cm.exception), 'UNIQUE constraint failed: '
+                                            'properties.defnID, '
+                                            'properties.propID')
+
+
 class TestLabelDatabaseInteraction(unittest.TestCase):
 
     def setUp(self):
